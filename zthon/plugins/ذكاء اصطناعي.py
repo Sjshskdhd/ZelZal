42b4f4b5d205ada0542563d2c052f03773c92b83
Zeinn0 <127055306+Zeinn0@users.noreply.github.com>
2023-04-20 22:53:36 +0200

    Add files via upload

---
+++ zthon/plugins/ذكاء اصطناعي.py
@@ -0,0 +1,51 @@
+""" 
+# 𓏺الهكر زين 
+# حقزق الهقر زين
+
+"""
+
+import asyncio
+
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.contacts import UnblockRequest as unblock
+from zthon import zedub
+
+from ..core.managers import edit_or_reply
+
+
+@zedub.zed_cmd(pattern="حساب العمر(?:\s|$)([\s\S]*)")
+async def song2(event):
+    card = event.pattern_match.group(1)
+    chat = "@xbank1_bot"
+    await reply_id(event)
+    zed = await edit_or_reply(event, "**- جـارِ معرفت عمرك بالتفصيل...**")
+    async with event.client.conversation(chat) as conv:
+        try:
+            await conv.send_message(card)
+        except YouBlockedUserError:
+            await zedub(unblock("xbank1_bot"))
+            await conv.send_message(card)
+        await asyncio.sleep(3)
+        response = await conv.get_response()
+        await event.client.send_read_acknowledge(conv.chat_id)
+        await event.client.send_message(event.chat_id, response.message)
+        await zed.delete()
+
+
+@zedub.zed_cmd(pattern="اسال(?:\s|$)([\s\S]*)")
+async def song2(event):
+    card = event.pattern_match.group(1)
+    chat = "@Az_12bot"
+    await reply_id(event)
+    zed = await edit_or_reply(event, "**- جـارِ الحصول علي اجابه ...**")
+    async with event.client.conversation(chat) as conv:
+        try:
+            await conv.send_message(card)
+        except YouBlockedUserError:
+            await zedub(unblock("Az_12bot"))
+            await conv.send_message(card)
+        await asyncio.sleep(20)
+        response = await conv.get_response()
+        await event.client.send_read_acknowledge(conv.chat_id)
+        await event.client.send_message(event.chat_id, response.message)
+        await zed.delete()
