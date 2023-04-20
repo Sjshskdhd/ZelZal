69c299d2dbbaf158763e6be0b32c96e23c385813
Zeinn0 <127055306+Zeinn0@users.noreply.github.com>
2023-04-20 14:52:25 +0200

    Add files via upload

---
+++ zthon/core/session.py
@@ -0,0 +1,52 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# CatUserBot #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# Copyright (C) 2020-2023 by Tgzedub@Github.
+
+# This file is part of: https://github.com/Tgzedub/catuserbot
+# and is released under the "GNU v3.0 License Agreement".
+
+# Please see: https://github.com/Tgzedub/catuserbot/blob/master/LICENSE
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+import sys
+
+from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
+from telethon.sessions import StringSession
+
+from ..Config import Config
+from .client import CatUserBotClient
+
+__version__ = "2.10.6"
+
+loop = None
+
+if Config.STRING_SESSION:
+    session = StringSession(str(Config.STRING_SESSION))
+else:
+    session = "catuserbot"
+
+try:
+    zedub = CatUserBotClient(
+        session=session,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        loop=loop,
+        app_version=__version__,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    )
+except Exception as e:
+    print(f"STRING_SESSION - {e}")
+    sys.exit()
+
+
+zedub.tgbot = tgbot = CatUserBotClient(
+    session="CatTgbot",
+    api_id=Config.APP_ID,
+    api_hash=Config.API_HASH,
+    loop=loop,
+    app_version=__version__,
+    connection=ConnectionTcpAbridged,
+    auto_reconnect=True,
+    connection_retries=None,
+).start(bot_token=Config.TG_BOT_TOKEN)
